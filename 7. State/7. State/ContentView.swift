3568148f00730f9b093f1a0978868956c8196669
vivalalova <vivalalova0@gmail.com>
2020-03-29 13:11:20 +0800

    button

--- 7. State/7. State/ContentView.swift
+++ 7. State/7. State/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
 
     @State private var count = 0
 
+    @State private var count2 = 0
+
     var body: some View {
         VStack {
             Button(action: {
@@ -28,6 +30,15 @@ struct ContentView: View {
             Btn(color: .red, count: $count)
             Btn(color: .black, count: $count)
             Btn(color: .green, count: $count)
+
+            Text("\(count2)")
+                .font(.system(size: 50))
+
+            HStack {
+                Btn(color: .red, count: $count2)
+                Btn(color: .black, count: $count2)
+                Btn(color: .green, count: $count2)
+            }
         }
     }
 }
@@ -42,15 +53,18 @@ struct Btn: View {
     var color: Color
     @Binding var count: Int
 
+    @State private var pressCount = 0
+
     var body: some View {
         Button(action: {
             // 切換播放與停止按鈕
             self.count += 1
+            self.pressCount += 1
         }) {
             Circle()
-                .frame(width: 150, height: 150)
+                .frame(width: 100, height: 100)
                 .overlay(
-                    Text("\(self.count)")
+                    Text("\(self.pressCount)")
                         .font(.system(size: 80, weight: .bold, design: .rounded))
                         .foregroundColor(.white)
                 ).foregroundColor(color)
